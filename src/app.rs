1210e1fde930633a3d0684c66df9a0920eda0590
Mahdi <mtzarrin@gmail.com>
2025-02-26 17:46:12 +0330

    add class `selected` and set the first item as teh selected session after deleting the selected session

--- src/app.rs
+++ src/app.rs
@@ -31,24 +31,33 @@ pub fn App() -> impl IntoView {
             .position(|session| session.get() == ss.get())
             .unwrap();
           let is_same_session = state.sessions().at_unkeyed(removing_index).get() == ss.get();
-          state.sessions().write().remove(removing_index);
           if is_same_session {
-            selected_session.set(None);
+            selected_session.set(state.sessions().into_iter().next().map(Into::into));
           }
+          state.sessions().write().remove(removing_index);
         });
     }>"Delete selected item"</button>
     <For each=move || state.sessions() key=|item| item.get() let(session)>
-      <p on:click=move |_| {
-        selected_session.set(Some(session.into()));
-      }>{move || session.field1().get()}</p>
+      <p
+        class:selected=move || {
+          selected_session
+            .read()
+            .is_some_and(|item| { *item.field0().read() == *session.field0().read() })
+        }
+
+        on:click=move |_| {
+          selected_session.set(Some(session.into()));
+        }
+      >
+        {move || session.field1().get()}
+      </p>
     </For>
 
     <strong>
       "Selected Item: "
       {move || {
         selected_session
-          .try_get()
-          .flatten()
+          .get()
           .map(|item| {
             view! { {move || item.field1().get()} }
           })
