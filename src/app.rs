d72b03caec172118f8d9a71efd0994837f9c45b5
Mahdi <mtzarrin@gmail.com>
2025-02-26 06:55:14 +0330

    greg's way modified

--- src/app.rs
+++ src/app.rs
@@ -1,73 +1,67 @@
-use leptos::wasm_bindgen;
-use leptos::web_sys;
-use leptos::{prelude::*, task::spawn_local};
-use reactive_stores::StoreField;
-use reactive_stores::{ArcField, ArcStore, Field, OptionStoreExt, Store};
-use std::ops::Deref;
-use std::ops::DerefMut;
-use std::time::Duration;
-use wasm_bindgen_futures::JsFuture;
-#[derive(Store, Default)]
+use leptos::prelude::*;
+use reactive_stores::{ArcField, ArcStore, Field, Store};
+#[derive(Store, Default, Clone)]
 pub struct State {
-  #[store(key:usize= |session| session.0)]
-  sessions: Vec<(usize, String)>,
-  selected_session: Option<Field<(usize, String)>>,
+  #[store(key:usize= |session| session.id)]
+  sessions: Vec<Session>,
 }
-pub async fn async_sleep(ms: i32) {
-  // Convert milliseconds to seconds for Duration
-  // let duration = Duration::from_millis(ms as u64);
-
-  // Use js_sys::Promise for setting up a timer
-  let promise = web_sys::js_sys::Promise::new(&mut |resolve, _| {
-    // Call JavaScript's setTimeout
-    let window = leptos::web_sys::window().unwrap();
-    window.set_timeout_with_callback_and_timeout_and_arguments_0(&resolve, ms).unwrap();
-  });
-
-  // Await the promise
-  JsFuture::from(promise).await.unwrap();
+#[derive(Store, Default, Clone)]
+pub struct Session {
+  id: usize,
+  label: String,
 }
-
 #[component]
 pub fn App() -> impl IntoView {
+  let selected_session: RwSignal<Option<Field<Session>>> = RwSignal::new(None);
+
   let state = Store::new(State {
-    sessions: vec![(0, "ItemA".to_string()), (1, "ItemB".to_string()), (2, "ItemC".to_string())],
-    selected_session: None,
+    sessions: vec![
+      Session { id: 0, label: "ItemA".to_string() },
+      Session { id: 1, label: "ItemB".to_string() },
+      Session { id: 2, label: "ItemC".to_string() },
+    ],
   });
   view! {
     <button on:click=move |_| {
-      state
-        .selected_session()
+      selected_session
         .get()
         .map(|selected_session| {
-          let selected = selected_session.read().0;
+          let selected = selected_session.read().id;
           let removing_index = state
             .sessions()
             .read()
             .iter()
-            .position(|session| session.0 == selected)
+            .position(|session| session.id == selected)
             .unwrap();
           state.sessions().write().remove(removing_index);
         });
-      state.selected_session().set(None);
+      selected_session.set(None);
     }>"Delete selected item"</button>
-    <For each=move || state.sessions() key=|item| item.get().0 let(session)>
+    <For each=move || state.sessions() key=|item| item.id().get() let(session)>
       <p on:click=move |_| {
-        state.selected_session().set(Some(session.into()));
-      }>{move || format!("{:?}", session.get())}</p>
+        selected_session.set(Some(session.into()));
+      }>{move || format!("{:?}", session.label().get())}</p>
     </For>
     <button on:click=move |_| {
-      if let Some(selected_session) = state.selected_session().get() {
-        let selected_session = selected_session.read().0;
+      if let Some(selected_session) = selected_session.get() {
+        let selected_session = selected_session.read().id;
         for session in state.sessions() {
-          if session.read().0 == selected_session {
-            session.write().deref_mut().1.push_str(" Hey!");
+          if session.read().id == selected_session {
+            session.write().label.push_str(" Hey!");
           }
         }
       }
     }>"Add Hey!"</button>
     <strong>
-      "Selected Item: " {move || { format!("{:?}", state.selected_session().get().get()) }}
+      "Selected Item: "
+      {move || { format!("{:?}", selected_session.get().map(|f| f.label()).get()) }}
     </strong>
+    {move || {
+      selected_session
+        .get()
+        .map(|ss| {
+          view! { <button disabled=move || ss.get().label.len() < 1>Test</button> }
+        })
+    }}
   }
 }
