005d1f65306bf7bdb18831ccb0ab01f919c7d307
Mahdi <mtzarrin@gmail.com>
2025-02-26 06:37:48 +0330

    with uuid

--- src/app.rs
+++ src/app.rs
@@ -1,72 +1,79 @@
 use leptos::prelude::*;
-use reactive_stores::{ArcField, ArcStore, Store};
-#[derive(Store, Default)]
+use reactive_stores::{Field, OptionStoreExt, Store, StoreFieldIterator};
+use uuid::Uuid;
+#[derive(Store, Default, Clone)]
 pub struct State {
-  #[store(key:String = |session| session.clone())]
-  sessions: Vec<String>,
-  #[store(skip)]
-  selected_session: Option<ArcField<String>>,
+  #[store(key:Uuid = |session| session.id)]
+  sessions: Vec<Session>,
+  selected_session: Option<Field<Session>>,
+}
+#[derive(Store, Default, PartialEq, Eq, Hash, Clone)]
+pub struct Session {
+  id: Uuid,
+  label: String,
 }
 
 #[component]
 pub fn App() -> impl IntoView {
-  let state = ArcStore::new(State {
-    sessions: vec!["Item1".to_string(), "Item2".to_string(), "Item3".to_string()],
+  let state = Store::new(State {
+    sessions: vec![
+      Session { id: Uuid::new_v4(), label: "Item1".to_string() },
+      Session { id: Uuid::new_v4(), label: "Item2".to_string() },
+      Session { id: Uuid::new_v4(), label: "Item3".to_string() },
+    ],
     selected_session: None,
   });
-  let state_cloned = state.clone();
   view! {
-    <button on:click={
-      let state_cloned = state.clone();
-      move |_| {
-        state_cloned
-          .update(|state| {
-            state
-              .selected_session
-              .clone()
-              .map(|selected_session| {
-                let removing_index = state
-                  .sessions
-                  .iter()
-                  .position(|session| *session == selected_session.get())
-                  .unwrap();
-                state.sessions.remove(removing_index);
-                state.selected_session = None;
-              });
-          })
-      }
+    <button on:click=move |_| {
+      state
+        .selected_session()
+        .map(|selected_session| {
+          let removing_index = state
+            .sessions()
+            .iter_unkeyed()
+            .position(|session| session.get() == selected_session.get().get())
+            .unwrap();
+          let is_same_session = state.sessions().at_unkeyed(removing_index).get()
+            == selected_session.get().get();
+          state.sessions().write().remove(removing_index);
+          if is_same_session {
+            state.selected_session().set(None);
+          }
+        });
     }>"Delete selected item"</button>
-    <For
-      each={
-        let state_cloned = state.clone();
-        move || state_cloned.clone().sessions()
-      }
-      key=|item| item.get()
-      let(session)
-    >
-      <p on:click={
-        let state_cloned = state.clone();
-        move |_| {
-          state_cloned.update(|state| state.selected_session = Some(session.clone().into()));
-        }
+    <ForEnumerate each=move || state.sessions() key=|item| item.get() let(index, session)>
+
+      <p on:click=move |_| {
+        state.selected_session().set(Some(session.into()));
       }>
-        {
-          let session_cloned = session.clone();
-          move || session_cloned.get()
-        }
+        <button
+          on:click=move |_| {
+            let is_session_selected = state
+              .selected_session()
+              .read()
+              .map(|f| *f.read() == *session.read())
+              .unwrap_or(false);
+            state.sessions().write().remove(index.get());
+            if is_session_selected {
+              state.selected_session().set(state.sessions().into_iter().next().map(Into::into));
+            }
+          }
+          class="fa fa-trash delete"
+        />
+        {move || session.get().label}
       </p>
-    </For>
+    </ForEnumerate>
 
     <strong>
       "Selected Item: "
       {move || {
-        format!(
-          "{:?}",
-          state_cloned
-            .with(|state| {
-              state.selected_session.clone().map(|selected_Session| selected_Session.get())
-            }),
-        )
+        state
+          .selected_session()
+          .try_get()
+          .flatten()
+          .map(|item| {
+            view! { <button disabled=move || item.label().get().len() < 1>Hey</button> }
+          })
       }}
     </strong>
   }
